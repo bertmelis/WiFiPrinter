74640ce2ce73d99365998d70b6f8c485b1cd76b1
Bert Melis <bertmelis@users.noreply.github.com>
2024-07-17 13:39:56 +0200

    CI test builds

---
+++ examples/simple-esp32/simple-esp32.ino
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiPrinter.h>
+
+const char* ssid = "........";
+const char* password = "........";
+
+void setup() {
+  Serial.begin(115200);
+  delay(5000);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+  }
+
+  WiFiPrinter.begin();
+
+  Serial.print("Ready for connections on ");
+  Serial.print(WiFi.localIP());
+  Serial.print(" - port 23\n");
+}
+
+void loop() {
+  static unsigned long lastMillis = 0;
+  if (millis() - lastMillis > 60 * 1000UL) {
+    lastMillis = millis();
+    Serial.printf("%lu- current free heap: %lu\n", lastMillis, ESP.getFreeHeap());
+    WiFiPrinter.printf("%lu- current free heap: %lu\n", lastMillis, ESP.getFreeHeap());
+  }
+  WiFiPrinter.handle();
+}
